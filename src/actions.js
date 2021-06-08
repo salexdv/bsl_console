1a3e481fff8d4ec3d72131c950db829b88220536
salexdv <salexdv@gmail.com>
2021-06-08 15:43:40 +0300

    Опции для отключения контекстного меню #175

--- src/actions.js
+++ src/actions.js
@@ -69,144 +69,149 @@ define(['vs/editor/editor.main'], function () {
     getActions = function(version1C) {
 
         let actions = {};
-        let overrideCopyPaste = true;
 
-        const verArray = version1C.split('.');
+        if (!editor.disableContextCommands) {
 
-        if (3 < verArray.length) {
-            overrideCopyPaste = !((8 <= parseInt(verArray[0])) && (3 <= parseInt(verArray[1])) && (18 <= parseInt(verArray[2])));
-        }
-        
-        overrideCopyPaste = false;
+            let overrideCopyPaste = true;
 
-        if (overrideCopyPaste) {
-            actions.copy_bsl = {
-                label: 'Копировать',
-                key: monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_C,
-                cmd: monaco.KeyMod.chord(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_C),
-                order: 1.1,
-                callback: function (ed) {
-                    selectionText = editor.getModel().getValueInRange(editor.getSelection());
-                    return null;
-                }
-            };            
-            actions.paste_bsl = {
-                label: 'Вставить',
-                key: monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_V,
-                cmd: monaco.KeyMod.chord(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_V),
-                order: 1.2,
-                callback: function (ed) {                
-                    setText(selectionText, null, false);
-                    return null;
-                }
-            };            
-        }
+            const verArray = version1C.split('.');
 
-        if (!DCSMode) {
-
-            actions.query_bsl = {
-                label: 'Конструктор запроса...',
-                key: monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_D,
-                cmd: monaco.KeyMod.chord(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_D),
-                order: 1.3,
-                callback: function (ed) {     
-                    sendEvent('EVENT_QUERY_CONSTRUCT', queryMode ? getText() : getQuery());
-                    return null;
-                }
-            };
-
-        }
-
-        actions.comment_bsl = {
-            label: 'Добавить комментарий',
-            key: monaco.KeyMod.CtrlCmd | monaco.KeyCode.NUMPAD_DIVIDE,
-            cmd: monaco.KeyMod.chord(monaco.KeyMod.CtrlCmd | monaco.KeyCode.NUMPAD_DIVIDE),
-            order: 1.5,
-            callback: function (ed) {                
-                addComment();
-                return null;
+            if (3 < verArray.length) {
+                overrideCopyPaste = !((8 <= parseInt(verArray[0])) && (3 <= parseInt(verArray[1])) && (18 <= parseInt(verArray[2])));
             }
-        };        
 
-        actions.uncomment_bsl = {
-            label: 'Удалить комментарий',
-            key: monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.NUMPAD_DIVIDE,
-            cmd: monaco.KeyMod.chord(monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.NUMPAD_DIVIDE),
-            order: 1.6,
-            callback: function (ed) {                
-                removeComment();
-                return null;
+            overrideCopyPaste = false;
+
+            if (overrideCopyPaste) {
+                actions.copy_bsl = {
+                    label: 'Копировать',
+                    key: monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_C,
+                    cmd: monaco.KeyMod.chord(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_C),
+                    order: 1.1,
+                    callback: function (ed) {
+                        selectionText = editor.getModel().getValueInRange(editor.getSelection());
+                        return null;
+                    }
+                };
+                actions.paste_bsl = {
+                    label: 'Вставить',
+                    key: monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_V,
+                    cmd: monaco.KeyMod.chord(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_V),
+                    order: 1.2,
+                    callback: function (ed) {
+                        setText(selectionText, null, false);
+                        return null;
+                    }
+                };
             }
-        };
-        
-        if (!queryMode && !DCSMode) {
 
-            actions.formatstr_bsl = {
-                label: 'Конструктор форматной строки...',
-                key: null,
-                cmd: null,
-                order: 1.4,
-                callback: function (ed) {     
-                    sendEvent('EVENT_FORMAT_CONSTRUCT', getFormatString());
-                    return null;
-                }
-            };        
+            if (!DCSMode && !editor.disableContextQueryConstructor) {
 
-            actions.format_bsl = {
-                label: 'Форматировать',
-                key: monaco.KeyMod.Alt | monaco.KeyMod.Shift | monaco.KeyCode.KEY_F,
-                cmd: monaco.KeyMod.chord(monaco.KeyMod.Alt | monaco.KeyMod.Shift | monaco.KeyCode.KEY_F),
-                order: 1.7,
-                callback: function (ed) {                
-                    editor.trigger('', 'editor.action.formatDocument');
-                    return null;
-                }
-            };  
+                actions.query_bsl = {
+                    label: 'Конструктор запроса...',
+                    key: monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_D,
+                    cmd: monaco.KeyMod.chord(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_D),
+                    order: 1.3,
+                    callback: function (ed) {
+                        sendEvent('EVENT_QUERY_CONSTRUCT', queryMode ? getText() : getQuery());
+                        return null;
+                    }
+                };
 
-            actions.wordwrap_bsl = {
-                label: 'Добавить перенос строки',                
-                order: 1.8,
-                callback: function (ed) {                
-                    addWordWrap();
-                    return null;
-                }
-            };
+            }
 
-            actions.unwordwrap_bsl = {
-                label: 'Удалить перенос строки',                
-                order: 1.8,
-                callback: function (ed) {                
-                    removeWordWrap();
-                    return null;
-                }
-            };
-
-        }
-
-        if (!DCSMode) {
-
-            actions.add_bookmark_bsl = {
-                label: 'Установить/удалить закладку',
-                key: monaco.KeyMod.Alt | monaco.KeyCode.F2,
-                cmd: monaco.KeyMod.chord(monaco.KeyMod.Alt | monaco.KeyCode.F2),
-                order: 1.9,
+            actions.comment_bsl = {
+                label: 'Добавить комментарий',
+                key: monaco.KeyMod.CtrlCmd | monaco.KeyCode.NUMPAD_DIVIDE,
+                cmd: monaco.KeyMod.chord(monaco.KeyMod.CtrlCmd | monaco.KeyCode.NUMPAD_DIVIDE),
+                order: 1.5,
                 callback: function (ed) {
-                    let line = getCurrentLine();
-                    updateBookmarks(line);
+                    addComment();
                     return null;
                 }
             };
 
-            actions.next_bookmark_bsl = {
-                label: 'Следующая закладка',
-                key: monaco.KeyCode.F2,
-                cmd: monaco.KeyMod.chord(monaco.KeyCode.F2),
-                order: 2.0,
+            actions.uncomment_bsl = {
+                label: 'Удалить комментарий',
+                key: monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.NUMPAD_DIVIDE,
+                cmd: monaco.KeyMod.chord(monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.NUMPAD_DIVIDE),
+                order: 1.6,
                 callback: function (ed) {
-                    goNextBookmark();
+                    removeComment();
                     return null;
                 }
             };
+
+            if (!queryMode && !DCSMode) {
+
+                actions.formatstr_bsl = {
+                    label: 'Конструктор форматной строки...',
+                    key: null,
+                    cmd: null,
+                    order: 1.4,
+                    callback: function (ed) {
+                        sendEvent('EVENT_FORMAT_CONSTRUCT', getFormatString());
+                        return null;
+                    }
+                };
+
+                actions.format_bsl = {
+                    label: 'Форматировать',
+                    key: monaco.KeyMod.Alt | monaco.KeyMod.Shift | monaco.KeyCode.KEY_F,
+                    cmd: monaco.KeyMod.chord(monaco.KeyMod.Alt | monaco.KeyMod.Shift | monaco.KeyCode.KEY_F),
+                    order: 1.7,
+                    callback: function (ed) {
+                        editor.trigger('', 'editor.action.formatDocument');
+                        return null;
+                    }
+                };
+
+                actions.wordwrap_bsl = {
+                    label: 'Добавить перенос строки',
+                    order: 1.8,
+                    callback: function (ed) {
+                        addWordWrap();
+                        return null;
+                    }
+                };
+
+                actions.unwordwrap_bsl = {
+                    label: 'Удалить перенос строки',
+                    order: 1.8,
+                    callback: function (ed) {
+                        removeWordWrap();
+                        return null;
+                    }
+                };
+
+            }
+
+            if (!DCSMode) {
+
+                actions.add_bookmark_bsl = {
+                    label: 'Установить/удалить закладку',
+                    key: monaco.KeyMod.Alt | monaco.KeyCode.F2,
+                    cmd: monaco.KeyMod.chord(monaco.KeyMod.Alt | monaco.KeyCode.F2),
+                    order: 1.9,
+                    callback: function (ed) {
+                        let line = getCurrentLine();
+                        updateBookmarks(line);
+                        return null;
+                    }
+                };
+
+                actions.next_bookmark_bsl = {
+                    label: 'Следующая закладка',
+                    key: monaco.KeyCode.F2,
+                    cmd: monaco.KeyMod.chord(monaco.KeyCode.F2),
+                    order: 2.0,
+                    callback: function (ed) {
+                        goNextBookmark();
+                        return null;
+                    }
+                };
+
+            }
 
         }
         
