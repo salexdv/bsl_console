01c2bbc5e86a7b8a12ec4188867a854e5f605bb8
Alexander Shkuraev <salexdv@gmail.com>
2021-08-01 09:54:43 +0300

    findMatches -> regexp (getRangesForNesteBlock)

--- src/bsl_helper.js
+++ src/bsl_helper.js
@@ -4613,7 +4613,7 @@ class bslHelper {
 
 		let ranges = [];		
 		let match = null;
-		const matches = model.findMatches(regexp, false, true, false, null, true);		
+		const matches = Finder.findMatches(model, regexp);
 				
     	if (matches) {
 			
