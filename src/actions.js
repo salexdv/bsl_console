0af2022aad80c038d57547ab7108a80b806991ac
salexdv <salexdv@gmail.com>
2022-02-11 13:13:14 +0300

    Исправил возврат текста запроса для конструктора

--- src/actions.js
+++ src/actions.js
@@ -118,14 +118,14 @@ let getActions = function(version1C) {
         }
 
         if (!window.isDCSMode() && !window.editor.disableContextQueryConstructor) {
-
-            let query_text = window.isQueryMode() ? window.getText() : window.getQuery();
+            
             actions.query_bsl = {
                 label: 'Конструктор запроса...',
                 key: monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_D,
                 cmd: monaco.KeyMod.chord(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_D),
                 order: 1.3,
                 callback: function (ed) {
+                    let query_text = window.isQueryMode() ? window.getText() : window.getQuery();
                     window.sendEvent('EVENT_QUERY_CONSTRUCT', query_text);
                     return null;
                 }
