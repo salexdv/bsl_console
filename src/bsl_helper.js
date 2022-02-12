411e2635b74ead099202abecd824ad4edb640053
salexdv <salexdv@gmail.com>
2022-02-12 13:16:31 +0300

    fix getMetadataStackForVar

--- src/bsl_helper.js
+++ src/bsl_helper.js
@@ -2459,7 +2459,7 @@ class bslHelper {
 		let match = Finder.findPreviousMatch(this.model, '(' + varName + '\\s*=\\s*(.*?))\\.(.*)', position);
 
 		if (match && match.matches.length == pattern_match_count) {
-
+			
 			if (match.range.startLineNumber < position.lineNumber) {
 				
 				let source_var = match.matches[2];
@@ -2535,7 +2535,7 @@ class bslHelper {
 						previous_ref: false,
 						column: var_match.range.startColumn + varName.length
 					});
-					if (var_match.matches[1].indexOf('[') == 0)
+					if (var_match.matches[1] && var_match.matches[1].indexOf('[') == 0)
 						stack.push({
 							var: engLang ? 'get' : 'получить',
 							line: position.lineNumber,
