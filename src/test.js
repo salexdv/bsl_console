b59c82f9a656c1221ac6d8475ec6837c77f6d186
salexdv <salexdv@gmail.com>
2022-05-31 08:47:15 +0300

    Доработка тестов

--- src/test.js
+++ src/test.js
@@ -200,15 +200,15 @@ describe("Проверка автокомлита и подсказок реда
 
       it("проверка подсказки параметров для глобальной функции Найти(", function () {
         bsl = helper('Найти(');        
-        let suggestions = [];
-        let help = bsl.getCommonSigHelp(bslGlobals.globalfunctions);
+        let context = bsl.getLastSigMethod({});
+        let help = bsl.getCommonSigHelp(context, bslGlobals.globalfunctions);
         expect(help).to.have.property('activeParameter');
       });
 
       it("проверка подсказки параметров для глобальной функции Найти обернутой в функцию", function () {
         bsl = helper('СтрНайти(Найти(');
-        let suggestions = [];
-        let help = bsl.getCommonSigHelp(bslGlobals.globalfunctions);
+        let context = bsl.getLastSigMethod({});
+        let help = bsl.getCommonSigHelp(context, bslGlobals.globalfunctions);
         expect(help).to.have.property('activeParameter');
       });
 
@@ -451,9 +451,9 @@ describe("Проверка автокомлита и подсказок реда
       });
 
       it("проверка подсказки параметров для пользовательской функции МояФункция2", function () {
-        bsl = helper('МояФункция2');        
-        let suggestions = [];
-        let help = bsl.getCommonSigHelp(bslGlobals.customFunctions);
+        bsl = helper('МояФункция2(');        
+        let context = bsl.getLastSigMethod({});
+        let help = bsl.getCommonSigHelp(context, bslGlobals.customFunctions);
         expect(help).to.have.property('activeParameter');
       });
 
