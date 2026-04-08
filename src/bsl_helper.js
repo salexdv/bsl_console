4b8c6a34556c3e13f774b0ab4e3c5c85b0d5e2ac
salexdv <salexdv@gmail.com>
2026-04-08 18:43:10 +0300

    Доработка сворачивания областей close #339

--- src/bsl_helper.js
+++ src/bsl_helper.js
@@ -7043,7 +7043,7 @@ class bslHelper {
 		if (semicolon)
 			template += ';';
 
-		const endMatches = Finder.findMatches(model, '(?:^|\\b)?(' + endString + ') ?;');
+		const endMatches = Finder.findMatches(model, template);
 		let endMatch = null;
 
 		let structFound = false;
@@ -7085,8 +7085,55 @@ class bslHelper {
 	}
 
 	/**
-	 * Finds blocks like functions by regexp	 
-	 * 
+	 * Finds nested region blocks (#Region...#EndRegion)
+	 *
+	 * @param {ITextModel} current model of editor
+	 *
+	 * @returns {array} - array of folding ranges
+	 */
+	static getRangesForRegions(model) {
+
+		let ranges = [];
+		let stack = [];
+
+		const startPattern = /^\s*#\s*(?:область|region)(?=\s|;|$)/i;
+		const endPattern = /^\s*#\s*(?:конецобласти|endregion)(?=\s|;|$)/i;
+		const lineCount = model.getLineCount();
+
+		for (let lineNumber = 1; lineNumber <= lineCount; lineNumber++) {
+
+			const content = model.getLineContent(lineNumber);
+
+			if (startPattern.test(content)) {
+				stack.push(lineNumber);
+			}
+			else if (endPattern.test(content)) {
+
+				const startLineNumber = stack.pop();
+
+				if (startLineNumber && startLineNumber < lineNumber) {
+					ranges.push(
+						{
+							kind: monaco.languages.FoldingRangeKind.Region,
+							start: startLineNumber,
+							end: lineNumber
+						}
+					)
+				}
+
+			}
+
+		}
+
+		ranges.sort((left, right) => left.start - right.start || left.end - right.end);
+
+		return ranges;
+
+	}
+
+	/**
+	 * Finds blocks like functions by regexp
+	 *
 	 * @param {ITextModel} current model of editor
 	 * @param {string} regexp to detect block 	 
 	 * 
@@ -7182,7 +7229,7 @@ class bslHelper {
 			ranges = ranges.concat(this.getRangesForConstruction(model, "пока|while", "конеццикла|enddo", true));
 			ranges = ranges.concat(this.getRangesForConstruction(model, "для .*(?:по|из) .*|for .* (?:to|each) .*", "конеццикла|enddo", true));
 			ranges = ranges.concat(this.getRangesForConstruction(model, "если|if", "конецесли|endif", true));
-			ranges = ranges.concat(this.getRangesForConstruction(model, "#область|#region", "#конецобласти|#endregion", false));
+			ranges = ranges.concat(this.getRangesForRegions(model));
 			ranges = ranges.concat(this.getRangesForConstruction(model, "#если|#if", "#конецесли|#endif", false));
 		}
 
