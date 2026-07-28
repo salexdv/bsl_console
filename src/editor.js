4d829afcfef0295a9945e022d32c6cb063601dc4
salexdv <salexdv@gmail.com>
2026-07-28 17:02:22 +0300

    Исправлена ошибка сравнения текстов при пустом правом тексте.

--- src/editor.js
+++ src/editor.js
@@ -799,7 +799,7 @@ function updateDiffEditorOption(optionName, optionValue) {
 
 }
 
-window.compare = function (text, sideBySide, highlight, markLines = true, ignoreWhitespace = true) {
+window.compare = function (text="", sideBySide=true, highlight=true, markLines = true, ignoreWhitespace = true, newOriginalText = "") {
   
   let language_id = window.getCurrentLanguageId();
   let currentTheme = getCurrentThemeName();
@@ -813,8 +813,8 @@ window.compare = function (text, sideBySide, highlight, markLines = true, ignore
     hideStatusBar();
   }
 
-  if (text) {      
-    
+  if (text || newOriginalText) {
+
     if (language_id == 'xml') {
       language_id = 'xml';
       currentTheme = 'vs';
