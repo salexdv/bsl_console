572a1a0d04dc928daa033d5034b44c8420eca2e2
salexdv <salexdv@gmail.com>
2026-07-28 13:35:59 +0300

    ecmaScriptInfo

--- src/polyfills.js
+++ src/polyfills.js
@@ -314,3 +314,51 @@ if (_self.Element && _self.Element.prototype && typeof _self.Element.prototype.r
     }
   };
 }
+
+// Вывод версии ES от zeegin/ecmaScriptInfo
+var ecmaScriptInfo = (function () {
+    // () => { is not allowed
+    function getESEdition() {
+        var array = [];
+        switch (true) {
+            case !Array.isArray:
+                return 3;
+            case !window.Promise:
+                return 5;
+            case !array.includes:
+                return 6;
+            case !''.padStart:
+                return 7;
+            case !Promise.prototype.finally:
+                return 8;
+            case !window.BigInt:
+                return 9;
+            case !Promise.allSettled:
+                return 10;
+            case !''.replaceAll:
+                return 11;
+            case !array.at:
+                return 12;
+            default:
+                return 13;
+        }
+    }
+
+    function getESYear(edition) {
+        return {
+            3: 1999,
+            5: 2009
+        }[edition] || (2009 + edition); // nullish coalescing (??) is not allowed
+    }
+
+    var edition = getESEdition();
+    var year = getESYear(edition);
+
+    return {
+        edition: edition, // usually shortened [edition,]
+        year: year,       // usually shortened [year,]
+        text: 'Edition: ' + edition + ' | Year: ' + year
+    }
+})();
+
+console.info('ECMAScript', ecmaScriptInfo.text);
