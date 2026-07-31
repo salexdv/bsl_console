229169ff95a6f072740183e13c8102c1348db75f
salexdv <salexdv@gmail.com>
2026-07-31 16:07:15 +0300

    Поиск определения для функции, вызываемой внутри конструкции

--- src/test.js
+++ src/test.js
@@ -1377,7 +1377,8 @@ setTimeout(() => {
       });
 
       it("проверка поиска определения для функции, вызываемой внутри конструкции", function () {
-        const model = getModel('Функция СкопироватьРекурсивно(Источник)\n// Какой-то код\nКонецФункции\n\nСтруктура.Вставить(Обход.Ключ, СкопироватьРекурсивно(Обход.Значение));');
+        
+        let model = getModel('Функция СкопироватьРекурсивно(Источник)\n// Какой-то код\nКонецФункции\n\nСтруктура.Вставить(Обход.Ключ, СкопироватьРекурсивно(Обход.Значение));');
         let position = new monaco.Position(5, 33);
         bsl = new bslHelper(model, position);
         let locations = bsl.provideDefinition();
@@ -1386,6 +1387,12 @@ setTimeout(() => {
         bsl = new bslHelper(model, position);
         locations = bsl.provideDefinition();
         assert.equal(locations, null);
+
+        model = getModel('Структура.Вставить(Обход.Ключ, СкопироватьРекурсивно(Обход.Значение));\n\nФункция СкопироватьРекурсивно(Источник)\n// Какой-то код\nКонецФункции');
+        position =  new monaco.Position(1, 33);
+        bsl = new bslHelper(model, position);
+        locations = bsl.provideDefinition();
+        expect(locations).to.be.an('array').that.not.is.empty;
       })
 
     }
